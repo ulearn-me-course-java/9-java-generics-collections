c839da8e4fc7b01bf64d10012549836272022802
wisedog <psnetwork1138@gmail.com>
2019-04-22 14:38:14 +0500

    Task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,17 +1,14 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
 
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
+        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("UTF-8"));
         for (Set<String> anagram : anagrams) {
             System.out.println(anagram);
         }
@@ -19,6 +16,50 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+
+        TreeSet<String> anagram;
+        Set<TreeSet<String>> res = new TreeSet<>(Comparator.comparing(TreeSet::first));
+        List<String> input = new ArrayList<>();
+        List<String> usesWords = new ArrayList<>();
+
+        new BufferedReader(new InputStreamReader(inputStream, charset)).lines().forEach(x -> input.add(x));
+
+        for (int i = 0; i < input.size(); i++) {
+            String s1 = input.get(i);
+
+            if (s1.length() <= 3 || usesWords.contains(s1) || !s1.matches("^[а-яА-Я]+$")) { continue; }
+
+            char[] c1, c2;
+            anagram = new TreeSet<>();
+
+            for (int j = i + 1; j < input.size(); j++) {
+                String s2 = input.get(j);
+
+                s1 = s1.toLowerCase();
+                s2 = s2.toLowerCase();
+
+                if(s1.equals(s2)){ break; }
+
+                c1 = s1.toCharArray();
+                c2 = s2.toCharArray();
+
+                Arrays.sort(c1);
+                Arrays.sort(c2);
+
+                if (String.valueOf(c1).equals(String.valueOf(c2))) {
+                    anagram.add(s2);
+                    usesWords.add(s2);
+                }
+            }
+
+            if (!anagram.isEmpty()) {
+                anagram.add(s1);
+                res.add(anagram);
+            }
+
+            usesWords.add(s1);
+        }
+
+        return new ArrayList<>(res);
     }
 }
