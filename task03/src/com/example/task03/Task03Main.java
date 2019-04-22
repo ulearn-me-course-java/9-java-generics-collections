f35f82f8f87c5fa7cf804e1dbcdbc0d7841e80e4
Platonov2 <jek1999060@mail.ru>
2019-04-22 17:16:11 +0500

    Платонов Евгений. Задание 03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -4,8 +4,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class Task03Main {
 
@@ -19,6 +19,58 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        List<Set<String>> answer = new ArrayList<>();
+        LinkedList<TreeSet<String>> list = new LinkedList<>();
+        Scanner scanner = new Scanner(inputStream);
+
+        while (scanner.hasNext()){
+            String tempStr = scanner.next().toLowerCase();
+
+            if (Pattern.matches(".*\\p{InCyrillic}.*", tempStr) && !tempStr.contains("-") && tempStr.length() >= 3){
+                TreeSet<String> tempSet = findSet(list, tempStr);
+                list.remove(tempSet);
+                tempSet.add(tempStr);
+                addSetInList(list, tempSet);
+            }
+        }
+
+        for (TreeSet<String> set : list) {
+            if (set.size() >= 2){
+                answer.add(set);
+            }
+        }
+
+        return answer;
+    }
+
+    private static TreeSet<String> findSet(LinkedList<TreeSet<String>> list, String string){
+        //Метод находит подходящий сет и возвращает его.
+        for (TreeSet<String> set : list) {
+            String firstSetEl = (String)set.toArray()[0];
+            if (getHashCode(firstSetEl) == getHashCode(string)){
+                return set;
+            }
+        }
+        //Если сета не найдено, то возвращается новый.
+        return new TreeSet<>();
+    }
+
+    private static void addSetInList(LinkedList<TreeSet<String>> list, TreeSet<String > set){
+        for (TreeSet<String> listSet : list) {
+            if (listSet.first().compareTo(set.first()) > 0){
+                list.add(list.indexOf(listSet), set);
+                return;
+            }
+        }
+        list.addLast(set);
+    }
+
+    private static int getHashCode(String string){
+        //Хэш код одинаков для анаграмм.
+        int hashCode = 0;
+        for (char ch : string.toCharArray()) {
+            hashCode += Objects.hashCode(ch);
+        }
+        return hashCode;
     }
 }
