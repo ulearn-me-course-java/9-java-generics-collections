efcedffc4699ee4446d776fc2b512208b7928008
Henkka <henkkawar@gmail.com>
2019-04-24 19:37:06 +0500

    task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,11 +1,9 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task03Main {
 
@@ -19,6 +17,29 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Map<String, Set<String>> map = new TreeMap<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, charset))) {
+            bufferedReader
+                    .lines()
+                    .map(String::toLowerCase)
+                    .filter(i -> i.length() >= 3 && i.matches("^[а-я]*$"))
+                    .forEach(i -> {
+                        String sortedString = sortedString(i);
+                        map.computeIfAbsent(sortedString, k -> new TreeSet<>()).add(i);
+                        map.computeIfPresent(sortedString, (k, v) -> v).add(i);
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return map.values().stream()
+                .filter(i -> i.size() > 1)
+                .collect(Collectors.toList());
     }
+
+    private static String sortedString(String value) {
+        char[] el = value.toCharArray();
+        Arrays.sort(el);
+        return String.valueOf(el);
+    }
+
 }
