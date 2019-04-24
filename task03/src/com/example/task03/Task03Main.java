0d5c8301e6cf33e8c0d54dda693d2220c8b37066
Dementor911 <lhfujy7477@gmail.com>
2019-04-24 22:05:05 +0500

    Потапов Дмитрий. Задание 03.

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,11 +1,11 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import javafx.collections.transformation.SortedList;
+
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class Task03Main {
 
@@ -19,6 +19,50 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        TreeSet<String> anagram;
+
+        Set<TreeSet<String>> result = new TreeSet<>(Comparator.comparing(TreeSet::first));
+        List<String> inputList = new ArrayList<>();
+        List<String> wordsPassed = new ArrayList<>();
+
+        new BufferedReader(new InputStreamReader(inputStream, charset)).lines().forEach(inputList::add);
+
+        for (int i = 0; i < inputList.size(); i++) {
+            String str1 = inputList.get(i);
+
+            if (str1.length() < 3 || wordsPassed.contains(str1) ||
+                    !Pattern.matches(".*\\p{InCyrillic}.*", str1) || str1.contains("-")) {
+                continue;
+            }
+
+            char[] ch1, ch2;
+            anagram = new TreeSet<>();
+
+            for (int j = i + 1; j < inputList.size(); j++) {
+                String str2 = inputList.get(j);
+
+                str1 = str1.toLowerCase();
+                str2 = str2.toLowerCase();
+                if (str1.equals(str2)){
+                    break;
+                }
+                ch1 = str1.toCharArray();
+                ch2 = str2.toCharArray();
+                Arrays.sort(ch1);
+                Arrays.sort(ch2);
+                if (String.valueOf(ch1).equals(String.valueOf(ch2))) {
+                    wordsPassed.add(str2);
+                    anagram.add(str2);
+                }
+
+            }
+            if (!anagram.isEmpty()) {
+                anagram.add(str1);
+                result.add(anagram);
+            }
+            wordsPassed.add(str1);
+
+        }
+        return new ArrayList<>(result);
     }
 }
