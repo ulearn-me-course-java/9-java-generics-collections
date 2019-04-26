76c4ef6c833b117c8ceff220d2dc8fcfec88e028
shee4 <lilite71@gmail.com>
2019-04-26 07:53:51 +0500

    tasking

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,68 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
 
+    private File file;
+    private List<E> list;
+
     public SavedList(File file) {
+        try {
+            if (!file.createNewFile()){
+                try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(file))){
+                    this.file = file;
+                    this.list = (List<E>)inputStream.readObject();
+                }
+            }
+            else {
+                this.file = file;
+                this.list = new ArrayList<>();
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E elem = list.get(index);
+        list.set(index, element );
+        rewriteFile();
+        return elem;
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index, element);
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E elem = list.get(index);
+        list.remove(index);
+        rewriteFile();
+        return elem;
+    }
+
+    private void rewriteFile(){
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(file))){
+            outputStream.writeObject(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
