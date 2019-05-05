b9fb5f4b29c024c8b78772a405a93446ea618a57
Max <uue33@mail.ru>
2019-05-05 17:16:23 +0500

    Иконников. Задание 03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,11 +1,11 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.io.*;
+import java.util.stream.Collectors;
 
 public class Task03Main {
 
@@ -15,10 +15,28 @@ public class Task03Main {
         for (Set<String> anagram : anagrams) {
             System.out.println(anagram);
         }
-
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Map<String, Set<String>> map = new TreeMap<>();// красно-черное дерево
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, charset))) {
+            reader
+                    .lines()
+                    .map(String::toLowerCase)//промежуточный поток, переведенный внижний регистр
+                    .filter(str -> str.length() > 2 && str.matches("[а-я]*"))//только русские буквы и больше 3 символов
+                    .forEach(str -> {
+                        char[] ch = str.toCharArray();
+                        Arrays.sort(ch);
+                        String sortedLetters = new String(ch);//отсортированные буквы в слове
+                        map.computeIfAbsent(sortedLetters, k -> new TreeSet<>()).add(str);//создать ключ, если такого нет
+                    });
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        //каждый набор должен состоять минимум из 2 слов
+        return map.values().stream()
+                .filter(setList -> setList.size() > 1)
+                .collect(Collectors.toList());
     }
 }
