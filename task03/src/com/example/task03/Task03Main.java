9e35243128b51301ada9431e44307052ddae8b96
AllaOsipyan <allosipyan@mail.ru>
2019-05-06 23:14:16 +0500

    Осипян Пайцар. Задание03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,11 +1,9 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task03Main {
 
@@ -19,6 +17,39 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+
+        Set<TreeSet<String>> anagrams = new TreeSet<>(Comparator.comparing(TreeSet::first));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, charset));
+
+        List<String> lines = reader.lines().collect(Collectors.toList());
+        while (lines.size() != 0) {
+            int i = 0;
+            TreeSet<String> anagram = new TreeSet<>();
+            anagram.add(lines.get(0).toLowerCase());
+            for (int j = 1; j < lines.size(); j++) {
+                String curr = lines.get(j);
+                if (curr.length() >= 3 && curr.toLowerCase().matches("^[а-я]*$")) {
+                    if ((curr.length() == anagram.first().length()
+                            && sort(anagram.first()).equals(sort(curr.toLowerCase())))) {
+                        anagram.add(lines.get(j).toLowerCase());
+                        lines.remove(j);
+                    }
+                } else {
+                    lines.remove(j);
+                }
+
+            }
+            if (anagram.size() >= 2)
+                anagrams.add(anagram);
+            lines.remove(i);
+        }
+
+        return new ArrayList<>(anagrams);
+    }
+
+    private static String sort(String str) {
+        char[] el = str.toLowerCase().toCharArray();
+        Arrays.sort(el);
+        return new String(el);
     }
 }
