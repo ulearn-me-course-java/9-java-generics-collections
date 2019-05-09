91f1d85a45d096093bc8f7f03cfa5b91783f2e12
uytera <uytera12@yandex.ru>
2019-05-09 09:47:48 +0500

    Даниил Бакулин. Задание02

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,69 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
+    private ArrayList<E> list = new ArrayList<>();
+    private File file;
+
 
     public SavedList(File file) {
+        try {
+            if (!file.createNewFile()) {
+                try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(file))) {
+                    this.file = file;
+                    list = (ArrayList<E>) inputStream.readObject();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                this.file = file;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateList() {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(file))) {
+            outputStream.writeObject(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E temp = list.get(index);
+        list.set(index, element);
+        updateList();
+        return temp;
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index, element);
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E temp = list.get(index);
+        list.remove(index);
+        updateList();
+        return temp;
     }
 }
