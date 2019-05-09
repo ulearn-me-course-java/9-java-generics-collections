38b2c551f318fe9c7150bf608229a2e0f9e6544a
Алексей Ступников <stuq11@gmail.com>
2019-05-09 15:44:23 +0500

    Задача 01 выполнена

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,46 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class Pair<TFirst, TSecond> {
+    private TFirst first;
+    private TSecond second;
+
+    private Pair(TFirst first, TSecond second){
+        this.first = first;
+        this.second = second;
+    }
+
+    TFirst getFirst() {
+        return first;
+    }
+
+    TSecond getSecond(){
+        return second;
+    }
+
+    public static <TFirst, TSecond> Pair<TFirst, TSecond> of (TFirst first, TSecond second){
+        return new Pair<TFirst, TSecond>(first, second);
+    }
+
+    public int hashCode() {
+        return (Objects.hashCode(this.first) << 16) ^ Objects.hashCode(this.second);
+    }
+
+    public boolean equals(Object object) {
+        if (object == null)
+            return false;
+
+        if (object.getClass() != this.getClass())
+            return false;
+
+        Pair<?, ?> pair = (Pair<?, ?>) object;
+        return Objects.equals(this.first, pair.first) && Objects.equals(this.second, pair.second);
+    }
+
+    public void ifPresent(BiConsumer<? super TFirst, ? super TSecond> consumer){
+        if ((this.first != null) && (this.second != null))
+            consumer.accept(this.first, this.second);
+    }
 }
