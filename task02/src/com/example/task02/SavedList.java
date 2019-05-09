295a5b548da960eb285198c779a5dbb10dd6b58a
Vitaliy Smichkovskiy <32594950+ProgressiveArt@users.noreply.github.com>
2019-05-09 23:32:42 +0300

    Задание 02 Смычковский В.
    # Задание 02 SavedList
    
    Реализуйте класс `SavedList`, представляющий собой список элементов с произвольным доступом,
    копия которого хранится в виде файла на жестком диске.
    
    - `SavedList` должен обновлять содержимое файла при каждом изменении списка элементов.
    
    - При создании нового экземпляра `SavedList` с указанием существующего файла,
    он должен загрузить список элементов из этого файла.
    
    - `SavedList` должен корректно обрабатывать ситуацию отсутствия файла
    (отсутствие файла означает отсутствие элементов в списке).
    
    - Для хранения списка в оперативной памяти можно использовать коллекции из `java.util`.
    
    - Для сохранения и загрузки элементов можно использовать `ObjectOutputStream` и `ObjectInputStream`.

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,63 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
 
+    private ArrayList<E> arrList;
+    private File file;
+
     public SavedList(File file) {
+
+        this.file = file;
+
+        if (file.isFile()) {
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+                arrList = (ArrayList<E>) ois.readObject();
+            } catch (ClassNotFoundException | IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            arrList = new ArrayList<>();
+        }
+    }
+
+    private void serialize() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+            oos.writeObject(arrList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return arrList.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E elem = arrList.set(index, element);
+        serialize();
+        return elem;
     }
 
     @Override
     public int size() {
-        return 0;
+        return arrList.size();
     }
 
     @Override
     public void add(int index, E element) {
+        arrList.add(index, element);
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E elem = arrList.remove(index);
+        serialize();
+        return elem;
     }
 }
--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -12,6 +12,11 @@ public class Task02Main {
         SavedList<String> list1 = new SavedList<>(file);
         list1.add("Z");
         System.out.println("list1: " + list1);
+        System.out.println("size: "  + list1.size());
+        list1.remove(1);
+        System.out.println("list1: " + list1);
+
+        System.out.println("get 0 - " + list1.get(0));
 
         SavedList<String> list2 = new SavedList<>(file);
         System.out.println("list2: " + list2);
