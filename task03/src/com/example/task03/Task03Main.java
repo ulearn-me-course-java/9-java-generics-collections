c76e8d273b47421a4f0ee6aac3a781de7f9433b6
uytera <uytera12@yandex.ru>
2019-05-09 11:35:05 +0500

    Даниил Бакулин. Задание03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,24 +1,64 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Pattern;
+
 
 public class Task03Main {
 
-    public static void main(String[] args) throws IOException {
+    public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
+        List<Set<String>> answer = new ArrayList<>();
+        Comparator<TreeSet<String>> comparator = Comparator.comparing(obj -> obj.first());
+        ArrayList<TreeSet<String>> array = new ArrayList<>();
+        Scanner scanner = new Scanner(inputStream);
 
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
-        for (Set<String> anagram : anagrams) {
-            System.out.println(anagram);
+        while (scanner.hasNext()) {
+            String tempStr = scanner.next().toLowerCase();
+
+
+            if (Pattern.matches(".*\\p{InCyrillic}.*", tempStr) && !tempStr.contains("-") && tempStr.length() >= 3) {
+                if(!findSet(array, tempStr)){
+                    TreeSet<String> temp = new TreeSet<>();
+                    temp.add(tempStr);
+                    array.add(temp);
+                }
+            }
         }
 
+        Collections.sort(array,comparator);
+
+        for (TreeSet<String> set : array) {
+            if (set.size() >= 2) {
+                answer.add(set);
+            }
+        }
+
+
+        return answer;
     }
 
-    public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+
+    private static boolean findSet( ArrayList<TreeSet<String>> list, String string) {
+        for (TreeSet<String> set : list) {
+            String firstSetEl = (String) set.toArray()[0];
+            if (getHashCode(firstSetEl) == getHashCode(string)) {
+                set.add(string);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int getHashCode(String string) {
+        //Хэш код одинаков для анаграмм.
+        int hashCode = 0;
+        for (char ch : string.toCharArray()) {
+            hashCode += Objects.hashCode(ch);
+        }
+        return hashCode;
     }
 }
