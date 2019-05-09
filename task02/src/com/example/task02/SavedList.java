621de10e737d513c6b7dd4366bf97733715da917
Саша <gorgonova@bk.ru>
2019-05-10 02:50:03 +0500

    Musina Alexandra task02

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,65 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
 import java.util.AbstractList;
+import java.io.*;
+import java.util.ArrayList;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
+    private ArrayList<E> list = new ArrayList<>();
+    private File file;
 
     public SavedList(File file) {
+        this.file = file;
+        try {
+            if (file.exists()) {
+                try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(file))) {
+                    this.file = file;
+                    this.list = (ArrayList<E>) inputStream.readObject();
+                }
+            } else {
+                this.file = file;
+                this.list = new ArrayList<>();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E el = list.set(index, element);
+        rewriteFile();
+        return el;
+    }
+
+    private void rewriteFile(){
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(file))){
+            outputStream.writeObject(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index, element);
+        rewriteFile();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E el = list.remove(index);
+        rewriteFile();
+        return el;
     }
 }
