5354dad78f129a140804f682074c7bab5d80ca46
Andrey Rakcheev <andre-orsk@yandex.ru>
2019-05-10 08:50:02 +0500

    Update SavedList.java

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,81 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
 
-    public SavedList(File file) {
+    private List<E> array;
+    private File file;
+
+    public SavedList(File file) throws ClassNotFoundException {
+
+        this.file = file;
+
+        if (!file.exists()) {
+            array = new ArrayList<>();
+            return;
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+
+            array = (ArrayList<E>) ois.readObject();
+
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return array.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E value = array.get(index);
+
+        array.set(index, element);
+        save();
+
+        return value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return array.size();
     }
 
     @Override
     public void add(int index, E element) {
+        array.add(index, element);
+        save();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E value = array.get(index);
+
+        array.remove(index);
+        save();
+
+        return value;
+    }
+
+    private void save() {
+        try {
+            if (!file.exists())
+                file.createNewFile();
+
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
+            oos.writeObject(array);
+
+            oos.flush();
+            oos.close();
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
     }
 }
