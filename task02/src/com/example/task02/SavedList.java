a8ee43364ff66db2f8903297625bdddd79f9aa3b
Alexey <alexey.pashnin@mail.ru>
2019-07-08 20:44:13 +0400

    Пашнин Алексей. Задание 02.

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,63 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
-import java.util.AbstractList;
+import java.io.*;
+import java.util.*;
 
-public class SavedList<E extends Serializable> extends AbstractList<E> {
+public class SavedList<E extends Serializable> extends AbstractList<E> implements Serializable{
+    ArrayList<E> savedList;
+    File file;
 
     public SavedList(File file) {
+        if (!file.exists()) {
+            savedList = new ArrayList<>();
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+            }
+        } else {
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))){
+                savedList = ((SavedList<E>) ois.readObject()).savedList;
+            } catch (IOException | ClassNotFoundException exception) {
+            }
+        }
+        this.file = file;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return savedList.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E oldValue = savedList.set(index, element);
+        update();
+        return oldValue;
     }
 
     @Override
     public int size() {
-        return 0;
+        return savedList.size();
     }
 
     @Override
     public void add(int index, E element) {
+        savedList.add(index, element);
+        update();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E oldValue = savedList.remove(index);
+        update();
+        return oldValue;
+    }
+
+    private void update() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))){
+            oos.writeObject(this);
+        } catch (IOException exception) {
+
+        }
     }
 }
