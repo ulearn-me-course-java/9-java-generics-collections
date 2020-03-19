168616ebf1667a9aeff0a248c70e24dde23b3f4c
Grin <grinlikegreenbutgrin@gmail.com>
2020-03-19 20:04:48 +0500

    task3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -4,14 +4,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
 
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
+        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.defaultCharset());
         for (Set<String> anagram : anagrams) {
             System.out.println(anagram);
         }
@@ -19,6 +18,28 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
+        HashSet<String> set = parse(inputStream, Charset.defaultCharset());
         return null;
+    }
+
+    private static HashSet<String> parse(InputStream inputStream, Charset charset){
+        Scanner sc = new Scanner(inputStream).useDelimiter("\n");
+        HashSet<String> set = new HashSet<>();
+        while (sc.hasNext()){
+            String next = sc.next();
+            if(next.length() < 3 || !isCyrillic(next)) continue;
+            next = next.toLowerCase();
+            set.add(next);
+        }
+        return set;
+    }
+
+    public static boolean isCyrillic(String s) {
+        for (char a : s.toCharArray()) {
+            if (Character.UnicodeBlock.of(a) != Character.UnicodeBlock.CYRILLIC) {
+                return false;
+            }
+        }
+        return true;
     }
 }
