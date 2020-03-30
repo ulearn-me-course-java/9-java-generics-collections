842119fcbd950811421c6c18a8695a7571b57502
LordOfTheDeadlines <vedroid28@mail.ru>
2020-03-30 14:36:28 +0500

    Сидорчук Анастасия. 9.01

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,38 @@
 package com.example.task01;
-
-public class Pair {
+import java.util.function.BiConsumer;
+public class Pair<T, U> {
     // TODO напишите реализацию
+    private final T firstValue;
+    private final U secondValue;
+
+    private Pair(T firstValue, U secondValue) {
+        this.firstValue = firstValue;
+        this.secondValue=secondValue;
+    }
+
+    public T getFirst(){
+        return firstValue;
+    }
+
+    public U getSecond(){
+        return secondValue;
+    }
+
+    public static <T1,T2> Pair<T1,T2> of(T1 first, T2 second){
+        return new Pair<>(first,second);
+    }
+
+    public void ifPresent( BiConsumer consumer){
+        if(firstValue==null||secondValue==null)
+            return;
+        consumer.accept(firstValue,secondValue);
+    }
+
+    public boolean equals(Pair<T, U> pair){
+        return firstValue==pair.firstValue && secondValue==pair.secondValue;
+    }
+
+    public int hashCode(){
+        return firstValue.hashCode()+secondValue.hashCode()*31;
+    }
 }
