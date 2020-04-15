e72462269e403703a0781d9f2df988c50abb8120
MatveyTolpeev <61470886+MatveyTolpeev@users.noreply.github.com>
2020-04-16 03:01:23 +0500

    Update Pair.java

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,58 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public final class Pair<T, U> {
+
+    private final T first;
+
+    private final U second;
+
+
+    private Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+
+    public static <T, U> Pair<T, U> of(T first, U second) {
+        return new Pair<>(first, second);
+    }
+
+
+    public T getFirst() {
+        return first;
+    }
+
+
+    public U getSecond() {
+        return second;
+    }
+
+
+    public void ifPresent(BiConsumer<? super T, ? super  U> biConsumer) {
+        if (first != null && second != null)
+            biConsumer.accept(first, second);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof Pair)) {
+            return false;
+        }
+
+        Pair<?, ?> other = (Pair<?, ?>) obj;
+        return Objects.equals(first, other.first) && Objects.equals(second, other.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
 }
