ba19a4f0c33704c7a74e6ecf2487caa8b5a10c29
LordOfTheDeadlines <vedroid28@mail.ru>
2020-04-16 15:17:36 +0500

    Сидорчук Анастасия. 9.03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -12,11 +12,11 @@ public class Task03Main {
 
     public static void main(String[] args) throws IOException {
 
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
+      /**  List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
         for (Set<String> anagram : anagrams) {
             System.out.println(anagram);
         }
-
+       */
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
