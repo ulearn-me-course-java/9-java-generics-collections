6467a676bf200582bcecba2fd94517f3397caded
Id.Io.T <kdevyatkov-ilya@mail.ru>
2020-04-22 20:48:54 +0500

    task01

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,46 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.function.BiConsumer;
+
+public class Pair<F, S> {
+    private final F first;
+    private final S second;
+
+    private Pair(F first, S second){
+        this.first = first;
+        this.second = second;
+    }
+
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    public static <F, S> Pair<F, S> of(F first, S second){
+        return new Pair(first, second);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Pair)) return false;
+
+        Pair other = (Pair) obj;
+
+        return other.first.equals(this.first) && other.second.equals(this.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return first.hashCode() + second.hashCode() * 1023;
+    }
+
+    public void ifPresent(BiConsumer<? super F, ? super S> cons){
+        if(first != null && second != null)
+            cons.accept(first, second);
+    }
+
 }
