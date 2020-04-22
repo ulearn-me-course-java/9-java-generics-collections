2e8cb3900c6b2d69fbbb999311355502a753df64
Наби Гадимов <gadimovnabi@gmail.com>
2020-04-22 23:24:57 +0500

    Гадимов Наби. Задание 1

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,40 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.function.BiConsumer;
+
+public class Pair <T1, T2> {
+    private T1 first;
+    private T2 second;
+
+    private Pair(T1 first, T2 second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public T1 getFirst(){
+        return first;
+    }
+    public T2 getSecond(){
+        return second;
+    }
+
+    public boolean equals(Pair<T1, T2> pair){
+        return first == pair.first && second == pair.second;
+    }
+
+    public int hashCode(){
+        return first.hashCode() + second.hashCode();
+    }
+
+    static <T1, T2> Pair<T1, T2> of(T1 first1 , T2 second2 ){
+        return new Pair<>(first1, second2 );
+    }
+
+    public void ifPresent( BiConsumer consumer){
+        if(first == null || second == null){
+            return;
+        }
+        consumer.accept(first, second);
+    }
+
 }
