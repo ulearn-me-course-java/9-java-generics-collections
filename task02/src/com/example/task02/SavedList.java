7db25bd2fc411f8a5f4eb6f66b25dba8af76366c
Ekaterina <malyavkina-00@mail.ru>
2020-04-23 12:47:32 +0500

    Малявкина Екатерина Задание 2

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,66 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
 
-public class SavedList<E extends Serializable> extends AbstractList<E> {
+public class SavedList<E extends Serializable> extends AbstractList<E> implements Serializable{
+    ArrayList<E> savedList;
+    File file;
 
     public SavedList(File file) {
+        if (!file.exists()) {
+            savedList = new ArrayList<>();
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                //ignore
+            }
+        } else {
+            try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))){
+                savedList = ((SavedList<E>) objectInputStream.readObject()).savedList;
+            } catch (IOException | ClassNotFoundException exception) {
+                //ignore
+            }
+        }
+        this.file = file;
+    }
+
+    private void update() {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file))){
+            out.writeObject(this);
+        } catch (IOException exception) {
+            //ignore
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return savedList.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E el = savedList.set(index, element);
+        update();
+        return el;
     }
 
     @Override
     public int size() {
-        return 0;
+        return savedList.size();
     }
 
     @Override
     public void add(int index, E element) {
+        savedList.add(index, element);
+        update();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E el = savedList.remove(index);
+        update();
+        return el;
     }
 }
