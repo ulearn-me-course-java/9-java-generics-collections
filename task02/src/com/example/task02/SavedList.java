b708725587ffcc6f3b3a2f5fd80dcc2fd7514f3c
Ekaterina <slalam.ww@gmail.com>
2020-04-23 21:44:25 +0500

    Абрамкова, 9.02

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,73 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
 
-public class SavedList<E extends Serializable> extends AbstractList<E> {
+public class SavedList<E extends Serializable> extends AbstractList<E> implements Serializable {
+
+    ArrayList<E> list;
+    File file;
 
     public SavedList(File file) {
+
+        if (!file.exists()) {
+            list = new ArrayList<>();
+            try {
+                file.createNewFile();
+            } catch (IOException ex) {
+                System.out.print(ex.getMessage());
+            }
+        } else {
+            try (ObjectInputStream input = new ObjectInputStream(new FileInputStream(file))) {
+                list = ((SavedList<E>) input.readObject()).list;
+            } catch (Exception ex) {
+                System.out.print(ex.getMessage());
+            }
+        }
+
+        this.file = file;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+
+        //научите придумывать нормальные имена переменным пожалуйста
+        E elemen = list.set(index, element);
+        update();
+        return elemen;
+        //return list.set(index,element);
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index, element);
+        update();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E elemen = list.remove(index);
+        update();
+        return elemen;
+    }
+
+    private void update() {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file))) {
+            out.writeObject(this);
+        } catch (Exception ex) {
+
+        }
+
     }
 }
