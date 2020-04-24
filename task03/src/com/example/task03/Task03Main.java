cc1d2461740746503e212175ec92edb642418d8a
Ekaterina <slalam.ww@gmail.com>
2020-04-24 11:54:17 +0500

    Абрамкова, 9.03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,11 +1,8 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Task03Main {
 
@@ -19,6 +16,69 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
+        List<String> words = new ArrayList<>();
+
+        try {
+            String word;
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, charset));
+            while ((word = bufferedReader.readLine()) != null) {
+                if (!words.contains(word) && isWordCorrect(word))
+                    words.add(word.toLowerCase());
+            }
+        } catch (IOException e) {
+            //ignore
+        }
+
+        HashMap<String, TreeSet<String>> anagramsMap = new HashMap<>();
+        for (String word : words) {
+            String key = getHashSetKey(anagramsMap.keySet(), word);
+            if (key == null) {
+                TreeSet<String> set = new TreeSet<>();
+                set.add(word);
+                anagramsMap.put(word, set);
+            } else {
+                anagramsMap.get(key).add(word);
+            }
+        }
+
+        ArrayList<Set<String>> anagrams = new ArrayList<>();
+        for (String key : anagramsMap.keySet()) {
+            if (anagramsMap.get(key).size() > 1) {
+                anagrams.add(anagramsMap.get(key));
+            }
+        }
+        return anagrams;
+    }
+
+    private static boolean isWordCorrect(String word) {
+        if (word.length() < 3) {
+            return false;
+        }
+        for (int i = 0; i < word.length(); i++) {
+            if (!Character.UnicodeBlock.of(word.charAt(i)).equals(Character.UnicodeBlock.CYRILLIC)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static String getHashSetKey(Set<String> keySet, String word) {
+        for (String key : keySet) {
+            if (isAnagramTo(key, word)) {
+                return key;
+            }
+        }
         return null;
+    }
+
+    private static boolean isAnagramTo(String source, String resource) {
+        if (source.length() != resource.length()) {
+            return false;
+        }
+        char[] sourceChars = source.toCharArray();
+        char[] resourceChars = resource.toCharArray();
+        Arrays.sort(sourceChars);
+        Arrays.sort(resourceChars);
+        return Arrays.equals(sourceChars, resourceChars);
     }
 }
