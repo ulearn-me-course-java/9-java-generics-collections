666fef3be5aae10c7e9a529baa173a4b3fa4e3c7
PavelLediankin <pavel.lediankin@at.urfu.ru>
2020-12-01 18:29:02 +0500

    Ледянкин Павел. Задание 01

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,62 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.function.BiConsumer;
+
+public class Pair<F, S>
+{
+    private final F first;
+    private final S second;
+
+    private Pair(F first, S second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <F, S> Pair<F, S> of(F first, S second)
+    {
+        return new Pair<>(first, second);
+    }
+
+    public F getFirst()
+    {
+        return first;
+    }
+
+    public S getSecond()
+    {
+        return second;
+    }
+
+    public void ifPresent(BiConsumer<? super F, ? super S> pair)
+    {
+        if (first == null || second == null)
+            return;
+        pair.accept(first, second);
+    }
+
+    public boolean equals(Object obj)
+    {
+        if (obj == null || this.hashCode() != obj.hashCode())
+            return false;
+        if (this == obj)
+            return true;
+        if (getClass() != obj.getClass())
+            return false;
+        Pair<F, S> pair;
+        try
+        {
+            pair = (Pair<F, S>) obj;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+        return first.equals(pair.first) && second.equals(pair.second);
+    }
+
+    public int hashCode()
+    {
+        return 31 * first.hashCode() + second.hashCode();
+    }
 }
