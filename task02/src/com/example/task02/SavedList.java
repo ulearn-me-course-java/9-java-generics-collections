844c167a8a52de88d108e71313dad480d0101ca8
PavelLediankin <pavel.lediankin@at.urfu.ru>
2020-12-01 18:57:53 +0500

    Ледянкин Павел. Задание 02

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,81 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SavedList<E extends Serializable> extends AbstractList<E> {
+public class SavedList<E extends Serializable> extends AbstractList<E>
+{
+    private final File file;
+    private List<E> list;
 
-    public SavedList(File file) {
+    public SavedList(File file) throws IOException
+    {
+        this.file = file;
+        getListFromFile();
     }
 
     @Override
-    public E get(int index) {
-        return null;
+    public E get(int index)
+    {
+        return list.get(index);
     }
 
     @Override
-    public E set(int index, E element) {
-        return null;
+    public E set(int index, E element)
+    {
+        E result = list.set(index, element);
+        saveListToFile();
+        return result;
     }
 
     @Override
-    public int size() {
-        return 0;
+    public int size()
+    {
+        return list.size();
     }
 
     @Override
-    public void add(int index, E element) {
+    public void add(int index, E element)
+    {
+        list.add(index, element);
+        saveListToFile();
     }
 
     @Override
-    public E remove(int index) {
-        return null;
+    public E remove(int index)
+    {
+        E result = list.remove(index);
+        saveListToFile();
+        return result;
+    }
+
+    private void saveListToFile()
+    {
+        try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(file)))
+        {
+            os.writeObject(list);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void getListFromFile() throws IOException
+    {
+        list = new ArrayList<>();
+        if (file.exists())
+            try (ObjectInputStream os = new ObjectInputStream(new FileInputStream(file)))
+            {
+                list = (List<E>) os.readObject();
+            }
+            catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+        else
+            file.createNewFile();
     }
 }
