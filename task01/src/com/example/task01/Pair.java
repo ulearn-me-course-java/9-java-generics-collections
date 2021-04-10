089d7979adf87837a4b744b9889e77120ec69d53
Vlad <vladpiligrimov@gmail.com>
2021-04-10 11:03:06 +0500

    task01 complete

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,47 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class Pair<T1, T2> {
+    private final T1 _first;
+    private final T2 _second;
+
+    private Pair(T1 _first, T2 _second) {
+        this._first = _first;
+        this._second = _second;
+    }
+
+    public static <T1, T2> Pair<T1, T2> of(T1 first, T2 second) {
+        return new Pair<>(first, second);
+    }
+
+    public T1 getFirst() {
+        return _first;
+    }
+
+    public T2 getSecond() {
+        return _second;
+    }
+
+    public void ifPresent(BiConsumer<? super T1, ? super T2> consumer) {
+        if (_first != null && _second != null) {
+            consumer.accept(_first, _second);
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Pair<?, ?> pair = (Pair<?, ?>) obj;
+        return Objects.equals(_first, pair._first) && Objects.equals(_second, pair._second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_first, _second);
+    }
 }
