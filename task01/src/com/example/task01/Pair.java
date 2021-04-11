a688de93c3aa8e05ab5e9dabbfa2ad703991c2a7
Sofya <fiepaz03@gmail.com>
2021-04-11 21:12:23 +0500

    Task 9.1

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,45 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class Pair<T, M> {
+    private final T first;
+    private final M second;
+
+    private Pair(T first, M second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public M getSecond() {
+        return second;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Pair))
+            return false;
+        Pair<?, ?> other = (Pair<?, ?>) obj;
+        return Objects.equals(first, other.first)
+                && Objects.equals(second, other.second);
+    }
+
+    public int hashCode()  {
+        return Objects.hash(first, second);
+    }
+
+    public static <T, M> Pair<T, M> of(T first, M second) {
+        return new Pair<>(first, second);
+    }
+
+    public void ifPresent(BiConsumer<? super T, ? super M> biConsumer) {
+        if (first != null && second != null)
+            biConsumer.accept(first, second);
+    }
 }
