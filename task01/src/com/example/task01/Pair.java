22a8a9d3f164eb649d27eaf492eddb19d3c221ec
Wither87 <wither_87@mail.ru>
2021-04-12 23:48:37 +0500

    Крутолевич Сергей. Задача 9.1

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,47 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.function.BiConsumer;
+
+public class Pair<T, U> {
+    private final T tValue;
+    private final U uValue;
+
+    public static <T, U> Pair<T, U> of (T param1, U param2){
+        return new Pair(param1, param2);
+    }
+
+    public void ifPresent(BiConsumer consumer){
+        if (tValue != null && uValue != null) consumer.accept(tValue, uValue);
+    }
+
+    public T getFirst(){
+        return tValue;
+    }
+
+    public U getSecond(){
+        return uValue;
+    }
+
+    @Override
+    public int hashCode(){
+        final int prime = 37;
+        int result = 1;
+        result = result * prime * tValue.hashCode();
+        result = result * prime * uValue.hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+        if (!(obj instanceof Pair)) return false;
+
+        Pair p = (Pair) obj;
+        return tValue.equals(p.tValue) && uValue.equals(p.uValue);
+    }
+
+    private Pair(T tValue, U uValue) {
+        this.tValue = tValue;
+        this.uValue = uValue;
+    }
 }
