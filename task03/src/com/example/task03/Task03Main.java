82359aadf512c7f91659b395319df726ddc0e438
Sergey <sergstas0@gmail.com>
2021-04-15 17:25:04 +0500

    task3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -4,8 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Task03Main {
 
@@ -19,6 +18,55 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Map<String, Set<String>> result = new HashMap<>();
+        Scanner scanner = new Scanner(inputStream, charset.name());
+
+        while (scanner.hasNext()) {
+            String word = scanner.nextLine().toLowerCase();
+            if (!checkCorrectness(word))
+                continue;
+            String key = getKey(word);
+            if (!result.containsKey(key))
+                result.put(key, new LinkedHashSet<>());
+            result.get(key).add(word);
+        }
+        result = removeInvalidSets(result);
+        result = sort(result);
+        return new ArrayList<>(result.values());
+    }
+
+    private static String getKey(String token) {
+        char[] arr = token.toCharArray();
+        Arrays.sort(arr);
+        return Arrays.toString(arr);
+    }
+
+    private static boolean checkCorrectness(String token){
+        for (char c : token.toCharArray())
+            if (!Character.UnicodeBlock.of(c).equals(Character.UnicodeBlock.CYRILLIC)) return false;
+        return token.length() > 2;
+    }
+
+    private static Map<String, Set<String>> removeInvalidSets(Map<String, Set<String>> sets) {
+        Map<String, Set<String>> result = new HashMap<>();
+        for (String key : sets.keySet())
+            if (sets.get(key).size() > 1)
+                result.put(key, sets.get(key));
+        return result;
+    }
+
+    private static Map<String, Set<String>> sort(Map<String, Set<String>> sets) {
+        Map<String, Set<String>> innerSort = new HashMap<>();
+        for (String key : sets.keySet()) {
+            List<String> list = new ArrayList<>(sets.get(key));
+            list.sort(Comparator.naturalOrder());
+            innerSort.put(list.get(0), new LinkedHashSet<>(list));
+        }
+        List<String> keys = new ArrayList<>(innerSort.keySet());
+        keys.sort(Comparator.naturalOrder());
+        Map<String, Set<String>> result = new LinkedHashMap<>();
+        for (String key : keys)
+            result.put(key, innerSort.get(key));
+        return result;
     }
 }
