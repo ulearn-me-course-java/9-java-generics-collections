d8d2a580e98644ec70fe8ca2728bc4d37507ba11
rewqty <boss.dima0000@gmail.com>
2021-06-19 01:42:11 +0500

    Челноков Дмитрий task01

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,44 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
-}
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+    public class Pair<T, U> {
+        private final T valueT;
+        private final U valueU;
+
+        public static <T, U> Pair<T, U> of (T param1, U param2){
+            return new Pair(param1, param2);
+        }
+
+        public void ifPresent(BiConsumer consumer){
+            if (valueT != null && valueU != null) consumer.accept(valueT, valueU);
+        }
+
+        public T getFirst(){
+            return valueT;
+        }
+
+        public U getSecond(){
+            return valueU;
+        }
+
+        @Override
+        public int hashCode(){
+            return Objects.hash(valueT, valueU);
+        }
+
+        @Override
+        public boolean equals(Object obj){
+            if (this == obj) return true;
+            if (!(obj instanceof Pair)) return false;
+
+            Pair p = (Pair) obj;
+            return valueT.equals(p.valueT) && valueU.equals(p.valueU);
+        }
+
+        private Pair(T valueT, U uValue) {
+            this.valueT = valueT;
+            this.valueU = uValue;
+        }
+    }
