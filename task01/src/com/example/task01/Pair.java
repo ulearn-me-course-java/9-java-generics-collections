f7ea5c8198db42d4ef95adfc63e0e03fc6f15619
Anny <70793585+anyast103@users.noreply.github.com>
2022-03-23 16:40:44 +0300

    Task_9.1

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,50 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class Pair<TKey, TValue> {
+    TKey first;
+    TValue second;
+
+    private Pair(TKey key, TValue value) {
+        this.first = key;
+        this.second = value;
+    }
+
+    public TKey getFirst() {
+        return this.first;
+    }
+
+    public TValue getSecond() {
+        return this.second;
+    }
+
+    public static Pair of(Object f, Object s) {
+        return new Pair(f, s);
+    }
+
+    public void ifPresent(BiConsumer consumer) {
+        if(this.first != null && this.second != null) {
+            if(consumer != null) {
+                consumer.accept(first,second);
+                System.out.println(first);
+                System.out.println(second);
+            }
+        }
+    }
+    @Override
+    public boolean equals(Object o) {
+        Pair obj = (Pair)o;
+        if(o instanceof Pair) {
+            return this.hashCode() == o.hashCode() &&
+                    (this.first == obj.first && this.second == obj.second);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
 }
