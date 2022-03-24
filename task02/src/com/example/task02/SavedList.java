f9ab663e03b0b73ed7ddf1c665818c83663b9a4f
Anny <70793585+anyast103@users.noreply.github.com>
2022-03-24 09:27:05 +0300

    Task_9.2

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,67 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
-import java.util.AbstractList;
+import java.io.*;
+import java.util.*;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
+    private static final long serialVersionUID = 42L;
+    public List<E> list;
+    private String pathToFile;
 
-    public SavedList(File file) {
+    public SavedList(File file) throws IOException {
+
+        pathToFile = file.getAbsolutePath();
+
+        if (!file.exists()) {
+            list = new ArrayList<>();
+            new PrintWriter(pathToFile, "UTF-8").close();
+            updateFile();
+        } else {
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(pathToFile))) {
+                list = (ArrayList<E>) ois.readObject();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void updateFile() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(pathToFile))) {
+            oos.writeObject(list);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E setted = list.set(index, element);
+        updateFile();
+        return setted;
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index, element);
+        updateFile();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E removedEl = list.remove(index);
+        updateFile();
+        return removedEl;
     }
 }
