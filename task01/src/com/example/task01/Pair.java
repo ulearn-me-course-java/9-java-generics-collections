3990a6b10225c3477c09d0ec9adc635ac1fffeb6
Saveliy <savelussava@mail.ru>
2022-04-19 18:20:35 +0500

    task1. Кудрявцев Савелий

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,44 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class Pair<T, V> {
+
+    private T firstValue;
+    private V secondValue;
+
+    private Pair(T first, V second){
+        firstValue = first;
+        secondValue = second;
+    }
+
+    public static <T, V> Pair<T, V> of(T first, V second){ return new Pair<>(first, second);}
+
+    public T getFirst() {
+        return firstValue;
+    }
+
+    public V getSecond() {
+        return secondValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return firstValue.equals(pair.firstValue) && secondValue.equals(pair.secondValue);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstValue, secondValue);
+    }
+
+    public void ifPresent(BiConsumer<? super T, ? super V> consumer) {
+        if (firstValue != null && secondValue != null)
+            consumer.accept(firstValue, secondValue);
+    }
 }
