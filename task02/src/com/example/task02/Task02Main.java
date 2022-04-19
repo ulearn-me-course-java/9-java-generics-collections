f53d829deb94931c2d3a53ca9e15e6f749516a2b
IlyaIIS <ilya1221221@mail.ru>
2022-04-19 20:41:39 +0500

    Исмаков Илья - task 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -5,17 +5,23 @@ import java.io.IOException;
 
 public class Task02Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         File file = new File("./testlist.dat");
 
+        file.delete();
         SavedList<String> list1 = new SavedList<>(file);
         list1.add("Z");
         System.out.println("list1: " + list1);
 
         SavedList<String> list2 = new SavedList<>(file);
         System.out.println("list2: " + list2);
-
+        list2.add("B");
+        list2.set(0, "A");
+        System.out.println("list2: " + list2);
+        list2.remove(1);
+        SavedList<String> list3 = new SavedList<>(file);
+        System.out.println("list3: " + list3);
     }
 
 }
