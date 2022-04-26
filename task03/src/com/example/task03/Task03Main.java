436b006d8a3420977b11c26bd5e0963f41d761a6
nodlya <nadya-popova-02@mail.ru>
2022-04-26 19:52:02 +0500

    Попова Надежда задание 9.3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,11 +1,9 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task03Main {
 
@@ -19,6 +17,23 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Map<String, Set<String>> anagrams = new TreeMap<>();
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, charset))){
+            bufferedReader.lines()
+                    .map(String :: toLowerCase)
+                    .filter(s -> s.length() >= 3 && s.matches("[а-я]*"))
+                    .forEach(s->{
+                        char[] wordInChar = s.toCharArray();
+                        Arrays.sort(wordInChar);
+                        String sortedWord = new String(wordInChar);
+                        anagrams.computeIfAbsent(sortedWord, k -> new TreeSet<>()).add(s);
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return anagrams.values()
+                .stream()
+                .filter(l -> l.size() >= 2)
+                .collect(Collectors.toList());
     }
 }
