3485fe0640d32d8fa50d8350c6a6696402868e72
Sekou Samassa <71543096+sksamassa@users.noreply.github.com>
2022-04-27 01:50:32 +0500

    Update Pair.java

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -2,4 +2,53 @@ package com.example.task01;
 
 public class Pair {
     // TODO напишите реализацию
+    package com.example.task01;
+
+import java.util.Objects;
+
+public class Pair<T1, T2> {
+    private T1 first;
+    private T2 second;
+
+    public T1 getFirst() {
+        return first;
+    }
+
+    public T2 getSecond() {
+        return second;
+    }
+
+    private Pair(T1 first, T2 second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+
+        if (!(obj instanceof Pair))
+            return false;
+
+        Pair<?,?> other = (Pair<?,?>) obj;
+        return Objects.equals(first, other.first) && Objects.equals(second, other.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    public static <T1,T2> Pair<T1,T2> of(T1 first, T2 second){
+        return new Pair<>(first, second);
+    }
+
+    public void ifPresent( java.util.function.BiConsumer<? super T1, ? super T2> consumer){
+        if (first != null && second != null){
+            consumer.accept(first,second);
+        }
+    }
+}
+
 }
