5d2ac455b74da3ec8d5c16b0c405fa6a0a4326e4
Kseniya <barkas074@yandex.ru>
2022-04-26 19:12:16 +0500

    completed task02

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,61 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
 
+    private List<E> listInMemory = new ArrayList<>();
+    private File file;
+
     public SavedList(File file) {
+        this.file = file;
+        if (file.exists()) {
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(this.file))) {
+                listInMemory = (List<E>) ois.readObject();
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return listInMemory.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E e = listInMemory.set(index, element);
+        updateList();
+        return e;
     }
 
     @Override
     public int size() {
-        return 0;
+        return listInMemory.size();
     }
 
     @Override
     public void add(int index, E element) {
+        listInMemory.add(index, element);
+        updateList();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E e = listInMemory.remove(index);
+        updateList();
+        return e;
+    }
+
+    private void updateList() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(this.file))) {
+            oos.writeObject(listInMemory);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
