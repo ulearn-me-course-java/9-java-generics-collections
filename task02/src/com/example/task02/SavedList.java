64778aac609fe10be1785aa0ce5d1b5ee82a6aa5
Sekou Samassa <71543096+sksamassa@users.noreply.github.com>
2022-04-27 01:52:45 +0500

    Update SavedList.java

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -5,8 +5,26 @@ import java.io.Serializable;
 import java.util.AbstractList;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
-
+ ArrayList<E> list;
+    File file;
+    
     public SavedList(File file) {
+        if (!file.exists()) {
+            list = new ArrayList<>();
+            try {
+                file.createNewFile();
+            } catch (IOException ex) {
+                System.out.print(ex.getMessage());
+            }
+        } else {
+            try (ObjectInputStream input = new ObjectInputStream(new FileInputStream(file))) {
+                list = ((SavedList<E>) input.readObject()).list;
+            } catch (Exception ex) {
+                System.out.print(ex.getMessage());
+            }
+        }
+
+        this.file = file;
     }
 
     @Override
