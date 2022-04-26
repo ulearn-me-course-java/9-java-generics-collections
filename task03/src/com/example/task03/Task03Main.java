c2958564bf7248d2da4d96a69d1f11c4b5941336
Kseniya <barkas074@yandex.ru>
2022-04-26 22:50:51 +0500

    completed task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,16 +1,13 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
-
         List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
         for (Set<String> anagram : anagrams) {
             System.out.println(anagram);
@@ -19,6 +16,23 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Map<String, Set<String>> anagrams = new TreeMap<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, charset))) {
+            bufferedReader.lines()
+                    .map(String::toLowerCase)
+                    .filter(str -> str.length() >= 3 && str.matches("[а-я]*"))
+                    .forEach(str -> {
+                        char[] ch = str.toCharArray();
+                        Arrays.sort(ch);
+                        String sortedWord = new String(ch);
+                        anagrams.computeIfAbsent(sortedWord, k -> new TreeSet<>()).add(str);
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return anagrams.values()
+                .stream()
+                .filter(l -> l.size() >= 2)
+                .collect(Collectors.toList());
     }
 }
