5ce6008b9c0c17b043d24d3b921ec551afe10bb4
eva <eva27122002b@gmail.com>
2022-04-26 20:29:34 +0500

    added task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -4,14 +4,16 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class Task03Main {
+    private static final Pattern PATTERN = Pattern.compile("^[а-яё]+$");
 
     public static void main(String[] args) throws IOException {
 
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
+        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"),
+                                                                        Charset.forName("windows-1251"));
         for (Set<String> anagram : anagrams) {
             System.out.println(anagram);
         }
@@ -19,6 +21,48 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        Map<Integer, Set<String>> wordsBySize = getWordsBySize(inputStream, charset);
+
+        Map<String, Set<String>> result = new TreeMap<>();
+        for (Set<String> value : wordsBySize.values()) {
+            for (String s : value) {
+                char[] symbols = s.toCharArray();
+                Arrays.sort(symbols);
+                String key = new String(symbols);
+
+                Set<String> words;
+                if(result.containsKey(key)){
+                    words = result.get(key);
+                }
+                else{
+                    result.put(key, words = new TreeSet<>());
+                }
+                words.add(s);
+            }
+        }
+        return new ArrayList<>(result.values());
+    }
+
+    public static Map<Integer, Set<String>> getWordsBySize(InputStream inputStream, Charset charset) {
+        // Integer - количество букв в слове, Set<String> - слова
+        Map<Integer, Set<String>> wordsBySize = new HashMap<>();
+        Scanner scanner = new Scanner(inputStream, charset.name()).useDelimiter("\n");
+
+        while(scanner.hasNext()){
+            String line = scanner.next().toLowerCase();
+            if(line.length() > 2 && PATTERN.matcher(line).matches()){
+                Set<String> words;
+                if(wordsBySize.containsKey(line.length())){
+                    words = wordsBySize.get(line.length());
+                }
+                else{
+                    wordsBySize.put(line.length(), words = new HashSet<>());
+                }
+                words.add(line);
+            }
+        }
+
+        wordsBySize.entrySet().removeIf(integerSetEntry -> integerSetEntry.getValue().size() < 2);
+        return wordsBySize;
     }
 }
