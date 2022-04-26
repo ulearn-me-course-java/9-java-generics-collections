c7c491b618edf26daf7f3416a6aa1f97b46cf39c
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-04-27 01:47:33 +0500

    Бабин Task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -7,15 +7,22 @@ public class Task02Main {
 
     public static void main(String[] args) throws IOException {
 
-        File file = new File("./testlist.dat");
+            File file = new File("./testlist.dat");
 
-        SavedList<String> list1 = new SavedList<>(file);
-        list1.add("Z");
-        System.out.println("list1: " + list1);
+            file.delete();
+            SavedList<String> list1 = new SavedList<>(file);
+            list1.add("Z");
+            System.out.println("list1: " + list1);
 
-        SavedList<String> list2 = new SavedList<>(file);
-        System.out.println("list2: " + list2);
+            SavedList<String> list2 = new SavedList<>(file);
+            System.out.println("list2: " + list2);
+
+            list2.add("B");
+            list2.set(0, "A");
+            System.out.println("list2: " + list2);
+            list2.remove(1);
+            SavedList<String> list3 = new SavedList<>(file);
+            System.out.println("list3: " + list3);
 
     }
-
 }
