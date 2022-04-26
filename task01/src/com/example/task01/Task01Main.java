230d7c93df7e43b44f5a2d3cdaa0d8fa24944996
ArsaGit <artyom.arsalanov@gmail.com>
2022-04-27 03:54:07 +0500

    Арсаланов Артём. Задание 9.1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,13 +2,14 @@ package com.example.task01;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Optional;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
 
         // TODO С корректно реализованным классом Pair должен компилироваться и успешно работать следующий код:
 
-        /*
+
         Pair<Integer, String> pair = Pair.of(1, "hello");
         Integer i = pair.getFirst(); // 1
         String s = pair.getSecond(); // "hello"
@@ -21,7 +22,8 @@ public class Task01Main {
         Pair<Integer, String> pair2 = Pair.of(1, "hello");
         boolean mustBeTrue = pair.equals(pair2); // true!
         boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
-        */
+        System.out.println(mustBeTrue);
+        System.out.println(mustAlsoBeTrue);
 
     }
 
