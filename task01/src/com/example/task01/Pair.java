518facbd22dd45359f71e109af9e44158072fb5d
Dangje <Deplodoctor@gmail.com>
2022-11-20 23:21:44 +0500

    lab end

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,39 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+
+
+
+import java.util.function.BiConsumer;
+
+public class Pair<F,S> {
+    private F first;
+    private S second;
+
+    private Pair(F first, S second){
+        this.first = first;
+        this.second = second;
+    }
+    public F getFirst(){
+        return first;
+    }
+    public S getSecond(){
+        return second;
+    }
+    public boolean equals(Pair p){
+        return (p.getFirst().equals(first) && p.getSecond().equals(second));
+    }
+    @Override
+    public int hashCode(){
+        return first.hashCode()+second.hashCode();
+    }
+
+    public static  <F, S> Pair<F,S> of(F first, S second){
+        return new Pair<>(first, second);
+    }
+
+    public void ifPresent(BiConsumer biConsumer){
+        if (first != null && second != null){
+            biConsumer.accept(first, second);
+        }
+    }
 }
--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,69 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.AbstractList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
-
+    private List<E> data;
+    File file;
     public SavedList(File file) {
+        this.file = file;
+        data = readFile(file.toPath());
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return data.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E elem = data.set(index, element);
+        writeFile(file.toPath());
+        return elem;
     }
 
     @Override
     public int size() {
-        return 0;
+        return data.size();
     }
 
     @Override
     public void add(int index, E element) {
+        data.add(index, element);
+        writeFile(file.toPath());
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E elem = data.remove(index);
+        writeFile(file.toPath());
+        return elem;
+    }
+
+    private List<E> readFile(Path path){
+        List<E> list = new LinkedList<>();
+        Object obj;
+        try(ObjectInputStream inputStream = new ObjectInputStream(Files.newInputStream(path)) ) {
+            while ((obj = inputStream.readObject()) != null){
+                list.add((E)obj);
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+    private void writeFile(Path path){
+        try(ObjectOutputStream outputStream = new ObjectOutputStream(Files.newOutputStream(path))) {
+            for(E elem : data){
+                outputStream.writeObject(elem);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
