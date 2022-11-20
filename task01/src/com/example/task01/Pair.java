d30398b19a33a90fee5df4a3ca9a5d964fbc447b
Leonidova Alexandra <alexandraleonid2002@gmail.ru>
2022-11-20 14:27:59 +0500

    Модуль 9 Леонидова

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,48 @@
 package com.example.task01;
 
-public class Pair {
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class Pair <fT, sT> {
     // TODO напишите реализацию
+    private fT fValue;
+    private sT sValue;
+    private Pair(fT fValue, sT sValue){
+        this.fValue = fValue;
+        this.sValue = sValue;
+    }
+    public fT getFirst(){
+        return fValue;
+    }
+    public sT getSecond(){
+        return sValue;
+    }
+    public void setfValue(fT fValue){
+        this.fValue = fValue;
+    }
+    public void setsValue(sT sValue){
+        this.sValue = sValue;
+    }
+    public boolean equals(Object value){
+        if (this == value){
+            return true;
+        }
+        Pair<fT, sT> pair = (Pair<fT, sT>) value;
+        if(this.fValue == pair.fValue && this.sValue == pair.sValue){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public int hashCode(){
+        return Objects.hash(fValue, sValue);
+    }
+    public static <fT, sT> Pair<fT, sT> of(fT fV, sT sV){
+         return new Pair<>(fV, sV);
+    }
+     public void ifPresent(BiConsumer biConsumer){
+           if(this.fValue != null && this.sValue != null){
+               biConsumer.accept(fValue, sValue);
+           }
+     }
 }
--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,11 +1,12 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import jdk.nashorn.internal.runtime.regexp.joni.Matcher;
+
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Task03Main {
 
@@ -18,7 +19,37 @@ public class Task03Main {
 
     }
 
-    public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+    public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset){
+        Map<String, Set<String>> res = new TreeMap<>();
+        List<Set<String>> result = new ArrayList<>();
+        List<String> words = new ArrayList<>();
+      //  ArrayList<String> rWords = new ArrayList<>();
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]*");
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, charset))){
+            words = bufferedReader.lines().collect(Collectors.toList());
+            for (String word : words ){
+                if (word.length() >= 3 && pattern.matcher(word).matches() == true){
+                    //   rWords.add(word.toLowerCase());
+                    char[] b = word.toLowerCase().toCharArray();
+                    Arrays.sort(b);
+                    String key = new String(b);
+                    if (res.containsKey(key) == true){
+                        res.get(key).add(word.toLowerCase());
+                    }else{
+                        res.put(key, new TreeSet<>());
+                        res.get(key).add(word.toLowerCase());
+                    }
+                }
+            }
+            for (String key : res.keySet()){
+                if(res.get(key).size() >= 2){
+                    result.add(res.get(key));
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return result;
     }
 }
