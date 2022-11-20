f84b8ba530f77a7589e9e220f738e8babd6809d1
Boris <BacR009@yandex.ru>
2022-11-21 00:39:15 +0500

    Васильев Борис 9 лаба

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,45 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.function.BiConsumer;
+
+public class Pair<T,P> {
+
+    private T firstElem;
+
+    private P secondElem;
+    private Pair(T valueFirst, P valueSecond){
+        this.firstElem=valueFirst;
+        this.secondElem=valueSecond;
+    }
+    public static <T,P> Pair<T,P> of(T firstValue, P secondValue){
+        return new Pair<>(firstValue,secondValue);
+    }
+
+    public T getFirst(){
+        return firstElem;
+    }
+
+    public P getSecond(){
+        return secondElem;
+    }
+
+    @Override
+    public int hashCode() {
+        return firstElem.hashCode()*10000+secondElem.hashCode()/10000;
+    }
+
+    public void ifPresent(BiConsumer consumer) {
+        if (this.firstElem != null && this.secondElem != null) {
+            consumer.accept((T) this.firstElem, (P) this.secondElem);
+        }
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) object;
+        return this.firstElem.equals(pair.firstElem)&&this.secondElem.equals(pair.secondElem);
+    }
 }
+
