1d42af18c02c96e1f2e31e358427081e42ebcf0f
Mikhail Egorov <m-5321e@yandex.com>
2022-11-21 08:43:48 +0500

    Егоров Михаил. task02

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,63 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
+    private final File file;
+    private List<E> list;
 
     public SavedList(File file) {
+        this.file = file;
+
+        if (file.exists()){
+            try (ObjectInputStream stream = new ObjectInputStream(new FileInputStream(file))) {
+                this.list = (List<E>) stream.readObject();
+            } catch (ClassNotFoundException | IOException e){
+                e.printStackTrace();
+            }
+        } else {
+            this.list = new ArrayList<>();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return this.list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E value = this.list.set(index, element);
+        writeToFile();
+        return value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        this.list.add(index, element);
+        writeToFile();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E value = this.list.remove(index);
+        writeToFile();
+        return value;
+    }
+
+    private void writeToFile(){
+        try (ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(this.file))) {
+            stream.writeObject(this.list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
