f648f0e90fac81b7d048eae0968ae91f2a411f41
Amir <70907990+Amirka2@users.noreply.github.com>
2022-11-30 02:10:05 +0500

    tasks 01-03 done

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,43 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class Pair<T, V> {
+    private final T first;
+    private final V second;
+
+    public Pair(T first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+    public static <T, V> Pair<T,V> of(T first, V second) {
+        return new Pair<>(first, second);
+    }
+
+    public T getFirst() {
+        return first;
+    }
+    public V getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair)) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+
+    public void ifPresent(BiConsumer biConsumer) {
+        if (first != null && second != null)
+            biConsumer.accept(first, second);
+    }
 }
--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,24 +1,46 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task03Main {
 
     public static void main(String[] args) throws IOException {
 
-        List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
-        for (Set<String> anagram : anagrams) {
-            System.out.println(anagram);
+        Map<String, Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
+        for (Set<String> entry : anagrams.values()) {
+            Iterator<String> s = entry.iterator();
+            while (s.hasNext()) {
+                System.out.println(s.next());
+            }
+            System.out.println();
         }
 
     }
 
-    public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+    public static Map<String, Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
+        Map<String, Set<String>> resultMap = new HashMap();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, charset))) {
+            List<String> words = br.lines()
+                    .map(String::toLowerCase)
+                    .filter(x -> x.length() > 2)
+                    .filter(x -> x.matches("[а-яё]+"))
+                    .collect(Collectors.toList());
+            for (String word: words) {
+                char[] letters = word.toCharArray();
+                Arrays.sort(letters);
+                String key = new String(letters);
+                resultMap.computeIfAbsent(key, set -> new TreeSet<>()).add(word);
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+
+        resultMap.values().removeIf(value -> value.size() < 2);
+
+        return resultMap;
     }
 }
