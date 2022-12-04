89cfe61020de9f94b2147d9497ad308e5034df76
Your Soul <timurnigmatulin6@gmail.com>
2022-12-05 03:46:36 +0500

    9.1-9.3

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,41 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import org.assertj.core.api.Assertions;
+
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class Pair <T1, T2> {
+    private final T1 first;
+    private final T2 second;
+    private Pair(T1 firstValue, T2 secondValue)
+    {
+        this.first = firstValue;
+        this.second = secondValue;
+    }
+    public static <T1,T2> Pair<T1,T2> of(T1 firstValue, T2 secondValue){
+        return new Pair<>(firstValue, secondValue);
+    }
+    public T1 getFirst(){
+        return this.first;
+    }
+    public T2 getSecond(){
+        return this.second;
+    }
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+        Pair<T1,T2> pair = (Pair<T1, T2>) obj;
+        return this.first.equals(pair.first) && this.second.equals(pair.second);
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(this.first,this.second);
+    }
+    public void ifPresent(BiConsumer consumer) {
+        if (this.first != null && this.second != null) {
+            consumer.accept((T1) this.first, (T2) this.second);
+        }
+    }
 }
