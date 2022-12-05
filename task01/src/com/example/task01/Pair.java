d90234f1077e2b36e2cda69ad0ed2a1434cd54ff
Alina <wbrflf3301@outlook.com>
2022-12-05 11:02:42 +0500

    Выполнено задание 1

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,43 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.*;
+import java.util.function.BiConsumer;
+
+public class Pair<T, V> {
+    public T first;
+    public V second;
+
+    private Pair(T firstValue, V secondValue) {
+        first = firstValue;
+        second = secondValue;
+    }
+
+    public static <T, V> Pair<T, V> of(T firstValue, V secondValue) {
+        return new Pair<>(firstValue, secondValue);
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public V getSecond() {
+        return second;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+        Pair<T, V> pair = (Pair<T, V>) obj;
+        return this.first.equals(pair.first) && this.second.equals(pair.second);
+    }
+
+    public int hashCode() {
+        return Objects.hash(this.first, this.second);
+    }
+
+    public void ifPresent(BiConsumer consumer) {
+        if (this.first != null && this.second != null) {
+            consumer.accept(this.first, this.second);
+        }
+    }
 }
