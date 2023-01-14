60a9fcb4679aacac09b80518a4cb7c34a57f16f6
msun_ <gmchofen@gmail.com>
2023-01-14 12:37:23 +0500

    java-9-MIhajlov
    Vipolnenie 9 temi.

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,73 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
 
+    private final File file;
+    private List<E> listElements;
+
     public SavedList(File file) {
+        if (!file.exists()) {
+            this.listElements = new ArrayList<>();
+        } else {
+            try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))) {
+                this.listElements = (List<E>) objectInputStream.readObject();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        this.file = file;
+    }
+
+    private void writeFile() {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(this.file))) {
+            objectOutputStream.writeObject(this.listElements);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return this.listElements.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E elem = this.listElements.set(index, element);
+        this.writeFile();
+        return elem;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.listElements.size();
     }
 
     @Override
     public void add(int index, E element) {
+        this.listElements.add(index, element);
+        this.writeFile();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E element = this.listElements.remove(index);
+        this.writeFile();
+        return element;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (E elem : this.listElements) {
+            sb.append(elem).append(" ");
+        }
+        return sb.toString();
     }
 }
+
