e58e52a99677881fc3c72b9c55a03cbc5d0d2667
Sug4chy <sb.9226981433@gmail.com>
2023-10-08 00:08:19 +0500

    Бойко Степан. Все задания

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,76 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
 
-    public SavedList(File file) {
+    private List<E> objects;
+    private final Path file;
+
+    public SavedList(File file) throws IOException {
+        this.file = file.toPath();
+        if (!file.exists()) {
+            Files.createFile(this.file);
+            objects = new ArrayList<>();
+        } else {
+            try (ObjectInputStream in = new ObjectInputStream(
+                    Files.newInputStream(this.file))) {
+                objects = ((ArrayList<E>) in.readObject());
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return objects.get(index);
     }
 
     @Override
     public E set(int index, E element) {
+        try (ObjectOutputStream out = new ObjectOutputStream(
+                Files.newOutputStream(file)))  {
+            E result = objects.set(index, element);
+            out.writeObject(objects);
+            return result;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return objects.size();
     }
 
     @Override
     public void add(int index, E element) {
+        try (ObjectOutputStream out = new ObjectOutputStream(
+                Files.newOutputStream(file))) {
+            objects.add(index, element);
+            out.writeObject(objects);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public E remove(int index) {
+        try(ObjectOutputStream out = new ObjectOutputStream(
+                Files.newOutputStream(file))) {
+            E result = objects.remove(index);
+            out.writeObject(objects);
+            return result;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 }
