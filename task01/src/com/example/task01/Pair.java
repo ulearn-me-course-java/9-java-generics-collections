af6af41b2a199d97e89e23e98aeedce8cf8cb4ab
FazylovV <vladislav.fazylov04@gmail.com>
2023-11-19 20:42:48 +0500

    task1 complete

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,43 @@
 package com.example.task01;
 
-public class Pair {
+import java.lang.reflect.Type;
+import java.net.Proxy;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class Pair<T1, T2> {
+    private T1 o1;
+    private T2 o2;
+
+    private Pair(T1 o1, T2 o2) {
+        this.o1 = o1;
+        this.o2 = o2;
+    }
+
+    public static <T1, T2> Pair<T1, T2> of(T1 o1, T2 o2) {
+        return new Pair<>(o1, o2);
+    }
+
+    public void ifPresent(BiConsumer biConsumer){
+        if(o1 == null || o2 == null) return;
+        biConsumer.accept(o1, o2);
+    }
+
+    public T1 getFirst() {
+        return o1;
+    }
+
+    public T2 getSecond() {
+        return o2;
+    }
+
+    public <T3, T4> boolean equals(Pair<T3, T4> pair) {
+        if(this == pair) return true;
+        return o1.equals(pair.getFirst()) && o2.equals(pair.getSecond());
+    }
+
+    public int hashCode() {
+        return Objects.hash(o1, o2);
+    }
     // TODO напишите реализацию
 }
