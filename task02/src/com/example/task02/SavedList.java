55b9c6f6cba95ebc3df39e5d6352d3a62c60ed32
Elizaveta Pernikova <lisapernikowa@mail.ru>
2023-11-19 18:37:49 +0500

    Task09.01-0.3

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,68 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
+    private List<E> list;
+    private File file;
 
     public SavedList(File file) {
+        this.file = file;
+        if(!file.exists()){
+            list = new ArrayList<>();
+        }
+        else{
+            listFromFile();
+        }
+    }
+
+    private void writeToFile(){
+        try(ObjectOutputStream objectOutputStream = new ObjectOutputStream(Files.newOutputStream(file.toPath()))){
+            objectOutputStream.writeObject(list);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void listFromFile(){
+        try(ObjectInputStream objectInputStream = new ObjectInputStream(Files.newInputStream(file.toPath()))){
+            list = (List<E>) objectInputStream.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E elem = list.set(index, element);
+        writeToFile();
+        return elem;
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index,element);
+        writeToFile();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E elem = list.remove(index);
+        writeToFile();
+        return elem;
     }
 }
--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,24 +1,58 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.lang.reflect.Array;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Task03Main {
-
+    static SortedSet<String> list;
     public static void main(String[] args) throws IOException {
 
         List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
         for (Set<String> anagram : anagrams) {
             System.out.println(anagram);
         }
-
     }
 
-    public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+    public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset){
+        Map<String, TreeSet<String>> map = new LinkedHashMap<>();
+        TreeSet<String> list = new TreeSet<>();
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, charset))) {
+            String str;
+            while((str = reader.readLine()) != null){
+                str = str.toLowerCase();
+                if(str.length() > 2 && str.matches("[а-яё]+")){
+                    list.add(str);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for(String elem : list){
+            char[] charArray = elem.toCharArray();
+            Arrays.sort(charArray);
+            String str = Arrays.toString(charArray);
+            if(map.containsKey(str)){
+                map.get(str).add(elem);
+            }
+            else{
+                map.put(str, new TreeSet<>());
+                map.get(str).add(elem);
+            }
+        }
+        return getList(map);
     }
+
+    private static List<Set<String>> getList(Map<String, TreeSet<String>> map){
+        List<Set<String>> anagrams = new ArrayList<>();
+        for(Map.Entry<String, TreeSet<String>> entry : map.entrySet()){
+            if(entry.getValue().size() >= 2){
+                anagrams.add(entry.getValue());
+            }
+        }
+        return anagrams;
+    }
+
 }
