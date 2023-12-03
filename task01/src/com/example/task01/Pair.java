8ee276d1bec742fbdae10796a131d51979615411
Kazmin Mihail <mihail.kazmin.04@mail.ru>
2023-12-03 23:54:06 +0500

    Казьмин М.М. ПрИ-201

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,51 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.function.BiConsumer;
+public class Pair<T1, T2>
+{
+    private T1 _first;
+    private T2 _second;
+
+    private Pair(T1 first, T2 second)
+    {
+        _first = first;
+        _second = second;
+    }
+
+    public static <T1, T2> Pair<T1, T2> of(T1 first, T2 second)
+    {
+        return new Pair<>(first, second);
+    }
+
+    public T1 getFirst()
+    {
+        return _first;
+    }
+
+    public T2 getSecond()
+    {
+        return _second;
+    }
+
+    public void ifPresent(BiConsumer<? super T1, ? super T2> consumer)
+    {
+        if(_first != null && _second != null)
+            consumer.accept(_first, _second);
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(_first, pair._first) && Objects.equals(_second, pair._second);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(_first, _second);
+    }
 }
--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -2,34 +2,74 @@ package com.example.task02;
 
 import java.io.File;
 import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
 
-public class SavedList<E extends Serializable> extends AbstractList<E> {
-
-    public SavedList(File file) {
+public class SavedList<E extends Serializable> extends AbstractList<E>
+{
+    private File DiskList;
+    private ArrayList<E> ArrList = new ArrayList<E>();
+    public SavedList(File file)
+    {
+       DiskList = file;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file)))
+        {
+            ArrList = (ArrayList<E>) ois.readObject();
+        } catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
-    public E get(int index) {
-        return null;
+    public E get(int index)
+    {
+        E sv = ArrList.get(index);
+        return sv;
     }
 
     @Override
-    public E set(int index, E element) {
-        return null;
+    public E set(int index, E element)
+    {
+        E sv = ArrList.set(index, element);
+        ListWrite();
+        return sv;
     }
 
     @Override
-    public int size() {
-        return 0;
+    public int size()
+    {
+        int sv = ArrList.size();
+        return sv;
     }
 
     @Override
-    public void add(int index, E element) {
+    public void add(int index, E element)
+    {
+        ArrList.add(index,element);
+        ListWrite();
     }
 
     @Override
-    public E remove(int index) {
-        return null;
+    public E remove(int index)
+    {
+        E sv = ArrList.remove(index);
+        ListWrite();
+        return sv;
+    }
+
+    public void ListWrite()
+    {
+        if(DiskList.exists())
+        {
+            DiskList.delete();
+        }
+        try (ObjectOutputStream ois = new ObjectOutputStream(new FileOutputStream(DiskList))) {
+            ois.writeObject(ArrList);
+        } catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 }
