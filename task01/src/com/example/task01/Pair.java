aaedc26824c6ac937e47a9bc5914741847bdd4e3
lenadekart <decartfamily@vk.com>
2024-02-10 23:00:59 +0500

    Generic for colletions изучены :D

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,42 @@
 package com.example.task01;
+import java.util.Objects;
+import java.util.function.BiConsumer;
 
-public class Pair {
-    // TODO напишите реализацию
+public class Pair<T1, T2> {
+    private final T1 firstValue;
+    private final T2 secondValue;
+
+    private Pair(T1 firstValue, T2 secondValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+    }
+
+    public static <T1, T2> Pair<T1, T2> of (T1 firstValue, T2 secondValue) {
+        return new Pair<>(firstValue, secondValue);
+    }
+
+    public T1 getFirst() {
+        return firstValue;
+    }
+
+    public T2 getSecond() {
+        return secondValue;
+    }
+
+    public void ifPresent(BiConsumer biConsumer) {
+        if (this.firstValue != null && this.secondValue != null) biConsumer.accept(this.firstValue, this.secondValue);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        Pair<T1, T2> pair = (Pair<T1, T2>) o;
+        return this.firstValue.equals(pair.firstValue) && this.secondValue.equals(pair.secondValue);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.firstValue, this.secondValue);
+    }
 }
--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,61 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.AbstractList;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
-
+    private List<E> savedList = new ArrayList<>();
+    private final File file;
     public SavedList(File file) {
+        if (file.exists()){
+            try (ObjectInputStream objectInputStream = new ObjectInputStream(Files.newInputStream(file.toPath()))){
+                this.savedList = (List<E>) objectInputStream.readObject();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        this.file = file;
+    }
+
+    private void writeFile() {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(Files.newOutputStream(file.toPath()))){
+            objectOutputStream.writeObject(this.savedList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return this.savedList.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E result = this.savedList.set(index, element);
+        this.writeFile();
+        return result;
     }
 
     @Override
     public int size() {
-        return 0;
+        return savedList.size();
     }
 
     @Override
     public void add(int index, E element) {
+        savedList.add(index, element);
+        this.writeFile();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E result = this.savedList.remove(index);
+        this.writeFile();
+        return result;
     }
 }
